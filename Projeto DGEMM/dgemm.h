3104ce4e0ec2b0df0563491cfbbf8ad9674bfb9f
josefn23 <166449723+josefn23@users.noreply.github.com>
2024-04-09 13:40:32 -0300

    Update dgemm.h

--- Projeto DGEMM/dgemm.h
+++ Projeto DGEMM/dgemm.h
@@ -1,13 +1,14 @@
 /*#include <x86intrin.h>*/
 
 void dgemm_1 (int n, double *A, double *B, double *C) {
+    double cij;
     for (int i = 0; i < n; ++i)
         for (int j = 0; j < n; ++j)
         {
-            double cij = C[i+j*n]; /* cij = C[i][j] */
+            cij = C[i+j*n]; /* cij = C[i][j] */
             for( int k = 0; k < n; k++ )
                 cij += A[i+k*n] * B[k+j*n]; /* cij += A[i][k]*B[k][j] */
-                C[i+j*n] = cij; /* C[i][j] = cij */
+            C[i+j*n] = cij; /* C[i][j] = cij */
         }
 }
 
