9e7782d76afcdf9fd246091f154c9d6670effe82
josefn23 <166449723+josefn23@users.noreply.github.com>
2024-04-10 12:30:07 -0300

    update dgemm.c e dgemm.h
    Rodando com as instruções avx (cap. 3). Por algum motivo no meu pc não ta funcionando o código do cap. 4. 
    Coloquei o tamanho da matriz n e numero de rodadas no #define pra facilitar.
    O comando pra compilar ta no começo do dgemm.c.

--- Projeto DGEMM/dgemm.c
+++ Projeto DGEMM/dgemm.c
@@ -1,36 +1,81 @@
+// compilando com: gcc -Wall -mavx512f -mavx512cd -o dgemm.exe dgemm.c
+
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include "dgemm.h"
 
-int main (void) {
+#define n       1024
+#define times   3
 
-    printf ("\ncomeco\n");
-    int n = 2048;
-    int aux1, aux2, aux3, aux4;
+int main (void) {
     
     double *A = malloc (n * n * sizeof (double));
     double *B = malloc (n * n * sizeof (double));
     double *C = malloc (n * n * sizeof (double));
-
+    int z;
     clock_t start, end;
-    double cpu_time_used;
+    double cpu_time_used, aux;
 
-/*
-    for (int i = 0; i < n; ++i)
+    printf ("\nDGEMM %i x %i", n, n);
+    printf ("\n\nConstruindo matriz");
+
+    srand (time(NULL));
+
+    for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j)
         {
-            A[i+j*n] = 2.56;
-            B[i+j*n] = 2.56;
+            A[i+j*n] = rand_double (0, 1000);
+            B[i+j*n] = rand_double (0, 1000);
         }
-*/
-    start = clock();
-    dgemm_1 (n, A, B, C);
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    }
 
-    printf ("\nCPU time: %f\n", cpu_time_used);
-    
+
+    printf ("\n\nExecutando DGEMM_1 (%ix)", times);
+    printf ("\n");
+    aux = 0;
+
+    for (z = 0; z < times; z++) {
+        start = clock();
+        dgemm_1 (n, A, B, C);
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        aux += cpu_time_used;
+        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+    }
+
+    printf ("\nMedia: %f sec", aux/times);
+
+    printf ("\n\nExecutando DGEMM_AVX (%ix)", times);
+    printf ("\n");
+    aux = 0;
+
+    for (z = 0; z < times; z++) {
+        start = clock();
+        dgemm_2 (n, A, B, C);
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        aux += cpu_time_used;
+        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+    }
+
+    printf ("\nMedia: %f sec", aux/times);
+
+    printf ("\n\nExecutando DGEMM_AVX_UNROLL (%ix)", times);
+    printf ("\n");
+    aux = 0;
+
+    for (z = 0; z < times; z++) {
+        start = clock();
+        dgemm_3 (n, A, B, C);
+        end = clock();
+        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+        aux += cpu_time_used;    
+        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+    }
+
+    printf ("\nMedia: %f sec", aux/times);
+
     free (A);
     free (B);
     free (C);
--- Projeto DGEMM/dgemm.h
+++ Projeto DGEMM/dgemm.h
@@ -1,5 +1,15 @@
-/*#include <x86intrin.h>*/
+#include <x86intrin.h>
+#define UNROLL 4
 
+double rand_double (double min, double max);
+double rand_double (double min, double max) {
+    double random = ((double) rand ()) / RAND_MAX;
+    double range = (max - min) * random;
+    double number = min + range;
+    return number;
+}
+
+void dgemm_1 (int n, double *A, double *B, double *C);
 void dgemm_1 (int n, double *A, double *B, double *C) {
     double cij;
     for (int i = 0; i < n; ++i)
@@ -12,17 +22,43 @@ void dgemm_1 (int n, double *A, double *B, double *C) {
         }
 }
 
-/*
+void dgemm_2 (size_t n, double* A, double* B, double* C);
 void dgemm_2 (size_t n, double* A, double* B, double* C) {
     for ( size_t i = 0; i < n; i+=4 )
         for ( size_t j = 0; j < n; j++ ) {
-            __m256d c0 = _mm256_load_pd(C+i+j*n); /* c0 = C[i][j] 
+            __m256d c0 = _mm256_load_pd(C+i+j*n); /* c0 = C[i][j] */
             for( size_t k = 0; k < n; k++ ) 
-                c0 = _mm256_add_pd(c0, /* c0 += A[i][k]*B[k][j] 
-                    _mm256_mul_pd(_mm256_load_pd(A+i+k*n),
-                    _mm256_broadcast_sd(B+k+j*n)));
-                _mm256_store_pd(C+i+j*n, c0); /* C[i][j] = c0 
+                c0 = _mm256_add_pd(c0, /* c0 += A[i][k]*B[k][j] */
+                     _mm256_mul_pd(_mm256_load_pd(A+i+k*n),
+                     _mm256_broadcast_sd(B+k+j*n)));
+            _mm256_store_pd(C+i+j*n, c0); /* C[i][j] = c0 */
             
         }
 }
-*/
+
+void dgemm_3 (int n, double* A, double* B, double* C);
+void dgemm_3 (int n, double* A, double* B, double* C) {
+  for (int i = 0; i < n; i+=UNROLL*8)
+    for (int j = 0; j < n; ++j){
+      printf ("\ndebug1");
+      printf ("\n");
+      __m512d c[UNROLL];
+      for (int r=0;r<UNROLL;r++){
+        printf ("\ndebug2");
+        printf ("\n");
+        c[r] = _mm512_load_pd(C+i+r*8+j*n); //[ UNROLL];
+        printf ("\ndebug3");
+        printf ("\n");
+      }
+
+      for( int k = 0; k < n; k++ ) {
+        __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B+j*n+k));
+        for (int r=0;r<UNROLL;r++)
+          c[r] = _mm512_fmadd_pd(_mm512_load_pd(A+n*k+r*8+i), bb, c[r]);
+      }
+
+      for (int r=0;r<UNROLL;r++)
+        _mm512_store_pd(C+i+r*8+j*n, c[r]);
+    }
+}
+
