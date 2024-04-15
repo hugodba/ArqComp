53cef6f389cd9ed0920d9b77924d900c115a8278
josefn23 <166449723+josefn23@users.noreply.github.com>
2024-04-15 18:51:11 -0300

    update dgemm.c dgemm.h
    Todos os códigos DGEMM funcionando no Windows 11

--- Projeto DGEMM/dgemm.c
+++ Projeto DGEMM/dgemm.c
@@ -1,83 +1,229 @@
-// compilando com: gcc -Wall -mavx512f -mavx512cd -o dgemm.exe dgemm.c
+// compilando com: gcc -march=native -mavx -mfma -fopenmp -o dgemm.exe dgemm.c
+// Windows 11
 
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 #include "dgemm.h"
 
-#define n       1024
-#define times   3
+#define n           1024
+#define times       1
+#define algorithm   0
+
+/*
+algorithm:
+ 1: DGEMM_1
+ 2: DGEMM_AVX
+ 3: DGEMM_AVX_UNROLL
+ 4: DGEMM_BLOCK_1
+ 5: DGEMM_AVX_UNROLL_BLOCK_2
+ 6: DGEMM_AVX_UNROLL_BLOCK_2_PARALLEL
+ <1 || >6: ALL
+*/
 
 int main (void) {
-    
-    double *A = malloc (n * n * sizeof (double));
-    double *B = malloc (n * n * sizeof (double));
-    double *C = malloc (n * n * sizeof (double));
     int z;
     clock_t start, end;
     double cpu_time_used, aux;
 
+    double *A = criar_matriz (n);
+    double *B = criar_matriz (n);
+    double *C = criar_matriz (n);
+
     printf ("\nDGEMM %i x %i", n, n);
-    printf ("\n\nConstruindo matriz");
 
-    srand (time(NULL));
+    switch (algorithm) {
+        case 1:
+            printf ("\n\nExecutando DGEMM_1 (%ix)", times);
+            printf ("\n");
+            aux = 0;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j)
-        {
-            A[i+j*n] = rand_double (0, 1000);
-            B[i+j*n] = rand_double (0, 1000);
-        }
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_1 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+
+        case 2:
+            printf ("\n\nExecutando DGEMM_AVX (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_2 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+
+        case 3:
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_3 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+
+        case 4:
+            printf ("\n\nExecutando DGEMM_BLOCK_1 (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_4 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+            
+        case 5:
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL_BLOCK_2 (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_5 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+
+        case 6:
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL_BLOCK_2_PARALLEL (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_6 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+        break;
+
+        default:
+            printf ("\n\nExecutando DGEMM_1 (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_1 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_1: %f sec", aux/times);
+
+            printf ("\n\nExecutando DGEMM_AVX (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_2 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_AVX: %f sec", aux/times);
+
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_3 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_AVX_UNROLL: %f sec", aux/times);
+
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL_BLOCK_1 (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_4 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_AVX_UNROLL_BLOCK_1: %f sec", aux/times);
+
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL_BLOCK_2 (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_5 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_AVX_UNROLL_BLOCK_2: %f sec", aux/times);
+
+            printf ("\n\nExecutando DGEMM_AVX_UNROLL_BLOCK_2_PARALLEL (%ix)", times);
+            printf ("\n");
+            aux = 0;
+
+            for (z = 0; z < times; z++) {
+                start = clock();
+                dgemm_6 (n, A, B, C);
+                end = clock();
+                cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+                aux += cpu_time_used;    
+                printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
+            }
+
+            printf ("\nMedia DGEMM_AVX_UNROLL_BLOCK_2_PARALLEL: %f sec", aux/times);
+
+            aux = -1;
+        break;
+
     }
 
-
-    printf ("\n\nExecutando DGEMM_1 (%ix)", times);
+    if (aux > 0)
+        printf ("\nMedia: %f sec", aux/times);
     printf ("\n");
-    aux = 0;
 
-    for (z = 0; z < times; z++) {
-        start = clock();
-        dgemm_1 (n, A, B, C);
-        end = clock();
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        aux += cpu_time_used;
-        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
-    }
+    free_matrizes (A, B, C);
 
-    printf ("\nMedia: %f sec", aux/times);
-
-    printf ("\n\nExecutando DGEMM_AVX (%ix)", times);
-    printf ("\n");
-    aux = 0;
-
-    for (z = 0; z < times; z++) {
-        start = clock();
-        dgemm_2 (n, A, B, C);
-        end = clock();
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        aux += cpu_time_used;
-        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
-    }
-
-    printf ("\nMedia: %f sec", aux/times);
-
-    printf ("\n\nExecutando DGEMM_AVX_UNROLL (%ix)", times);
-    printf ("\n");
-    aux = 0;
-
-    for (z = 0; z < times; z++) {
-        start = clock();
-        dgemm_3 (n, A, B, C);
-        end = clock();
-        cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-        aux += cpu_time_used;    
-        printf ("\nCPU time %i: %f sec", z + 1, cpu_time_used);
-    }
-
-    printf ("\nMedia: %f sec", aux/times);
-
-    free (A);
-    free (B);
-    free (C);
     return 0;
 }
--- Projeto DGEMM/dgemm.h
+++ Projeto DGEMM/dgemm.h
@@ -1,7 +1,11 @@
 #include <x86intrin.h>
-#define UNROLL 4
+#include <omp.h>
 
-double rand_double (double min, double max);
+#define UNROLL    4
+#define BLOCKSIZE 32
+#define THREADS   8
+
+double rand_double (double, double);
 double rand_double (double min, double max) {
     double random = ((double) rand ()) / RAND_MAX;
     double range = (max - min) * random;
@@ -9,7 +13,29 @@ double rand_double (double min, double max) {
     return number;
 }
 
-void dgemm_1 (int n, double *A, double *B, double *C);
+void rand_matrix (int, double *, int, int);
+void rand_matrix (int n, double *A, int min, int max) {
+
+  srand (time(NULL));
+
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j) 
+      A[i+j*n] = rand_double (min, max);
+}
+
+double *criar_matriz (int);
+double *criar_matriz (int n) {
+  double *matriz = malloc (n * n * sizeof (double));
+  rand_matrix (n, matriz, 0, 1000);
+  return matriz;
+}
+
+void free_matrizes (double *, double *, double *);
+void free_matrizes (double *A, double *B, double *C) {
+  free (A); free (B); free (C);
+}
+
+void dgemm_1 (int, double *, double *, double *);
 void dgemm_1 (int n, double *A, double *B, double *C) {
     double cij;
     for (int i = 0; i < n; ++i)
@@ -22,8 +48,8 @@ void dgemm_1 (int n, double *A, double *B, double *C) {
         }
 }
 
-void dgemm_2 (size_t n, double* A, double* B, double* C);
-void dgemm_2 (size_t n, double* A, double* B, double* C) {
+void dgemm_2 (size_t, double *, double *, double *);
+void dgemm_2 (size_t n, double *A, double *B, double *C) {
     for ( size_t i = 0; i < n; i+=4 )
         for ( size_t j = 0; j < n; j++ ) {
             __m256d c0 = _mm256_load_pd(C+i+j*n); /* c0 = C[i][j] */
@@ -36,29 +62,78 @@ void dgemm_2 (size_t n, double* A, double* B, double* C) {
         }
 }
 
-void dgemm_3 (int n, double* A, double* B, double* C);
-void dgemm_3 (int n, double* A, double* B, double* C) {
+void dgemm_3 (int, double *, double *, double *);
+void dgemm_3 (int n, double *A, double *B, double *C) {
   for (int i = 0; i < n; i+=UNROLL*8)
     for (int j = 0; j < n; ++j){
-      printf ("\ndebug1");
-      printf ("\n");
-      __m512d c[UNROLL];
+      __m256d c[UNROLL];
       for (int r=0;r<UNROLL;r++){
-        printf ("\ndebug2");
-        printf ("\n");
-        c[r] = _mm512_load_pd(C+i+r*8+j*n); //[ UNROLL];
-        printf ("\ndebug3");
-        printf ("\n");
+        c[r] = _mm256_load_pd(C+i+r*8+j*n); //[ UNROLL];
       }
 
       for( int k = 0; k < n; k++ ) {
-        __m512d bb = _mm512_broadcastsd_pd(_mm_load_sd(B+j*n+k));
+        __m256d bb = _mm256_broadcastsd_pd(_mm_load_sd(B+j*n+k));
         for (int r=0;r<UNROLL;r++)
-          c[r] = _mm512_fmadd_pd(_mm512_load_pd(A+n*k+r*8+i), bb, c[r]);
+          c[r] = _mm256_fmadd_pd(_mm256_load_pd(A+n*k+r*8+i), bb, c[r]);
       }
 
       for (int r=0;r<UNROLL;r++)
-        _mm512_store_pd(C+i+r*8+j*n, c[r]);
+        _mm256_store_pd(C+i+r*8+j*n, c[r]);
     }
 }
 
+void do_block_1 (int, int, int, int, double *, double *, double *);
+void do_block_1 (int n, int si, int sj, int sk, double *A, double *B, double *C) {
+  for (int i = si; i < si+BLOCKSIZE; ++i)
+    for (int j = sj; j < sj+BLOCKSIZE; ++j) {
+      double cij = C[i+j*n];/* cij = C[i][j] */
+      for( int k = sk; k < sk+BLOCKSIZE; k++ )
+        cij += A[i+k*n] * B[k+j*n];/* cij+=A[i][k]*B[k][j] */
+      C[i+j*n] = cij;/* C[i][j] = cij */
+    }
+}
+
+void do_block_2 (int, int, int, int, double *, double *, double *);
+void do_block_2 (int n, int si, int sj, int sk, double *A, double *B, double *C) {
+  for ( int i = si; i < si+BLOCKSIZE; i+=UNROLL*8 )
+    for ( int j = sj; j < sj+BLOCKSIZE; j++ ) {
+      __m256d c[UNROLL];
+      for (int r=0;r<UNROLL;r++)
+        c[r] = _mm256_load_pd(C+i+r*8+j*n); //[ UNROLL];
+
+      for( int k = sk; k < sk+BLOCKSIZE; k++ ) {
+        __m256d bb = _mm256_broadcastsd_pd(_mm_load_sd(B+j*n+k));
+        for (int r=0;r<UNROLL;r++)
+          c[r] = _mm256_fmadd_pd(_mm256_load_pd(A+n*k+r*8+i), bb, c[r]);
+      }
+
+      for (int r=0;r<UNROLL;r++)
+        _mm256_store_pd(C+i+r*8+j*n, c[r]);
+    }
+}
+
+void dgemm_4 (int, double *, double *, double *);
+void dgemm_4 (int n, double* A, double* B, double* C) {
+  for ( int sj = 0; sj < n; sj += BLOCKSIZE )
+    for ( int si = 0; si < n; si += BLOCKSIZE )
+      for ( int sk = 0; sk < n; sk += BLOCKSIZE )
+        do_block_1 (n, si, sj, sk, A, B, C);
+}
+
+void dgemm_5 (int, double *, double *, double *);
+void dgemm_5 (int n, double* A, double* B, double* C) {
+  for ( int sj = 0; sj < n; sj += BLOCKSIZE )
+    for ( int si = 0; si < n; si += BLOCKSIZE )
+      for ( int sk = 0; sk < n; sk += BLOCKSIZE )
+        do_block_2 (n, si, sj, sk, A, B, C);
+}
+
+void dgemm_6 (int, double *, double *, double *);
+void dgemm_6 (int n, double* A, double* B, double* C) {
+  omp_set_num_threads(THREADS);
+#pragma omp parallel for
+  for ( int sj = 0; sj < n; sj += BLOCKSIZE )
+    for ( int si = 0; si < n; si += BLOCKSIZE )
+      for ( int sk = 0; sk < n; sk += BLOCKSIZE )
+        do_block_2 (n, si, sj, sk, A, B, C);
+}
